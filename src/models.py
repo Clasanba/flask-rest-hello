73132a0a26179e72258a0d282919babdb0e1e85a
Clasanba <clasanba@gmail.com>
2022-08-19 19:47:24 +0000

    declared only db models

--- src/models.py
+++ src/models.py
@@ -7,13 +7,70 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    favorites = db.Column(db.Boolean(), db.ForeignKey('favorites.id')unique=False, nullable=False)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active,
+            "favorites": self.favorites,
+            # do not serialize the password, its a security breach
+        }
+        
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    status = db.Column(db.String(80), unique=False, nullable=False)
+    species = db.Column(db.Boolean(), unique=False, nullable=False)
+    gender = db.Column(db.Boolean(), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<Characters %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+            "gender": self.gender,
+        }
+        
+class Locations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    type = db.Column(db.String(80), unique=False, nullable=False)
+    dimension = db.Column(db.Boolean(), unique=False, nullable=False)
+
+def __repr__(self):
+        return '<Locations %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type,
+            "dimension": self.dimension,
+        }
+        
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    characters = db.Column(db.String(120), db.ForeignKey('characters.id'),unique=True, nullable=False)
+    locations = db.Column(db.String(80), db.ForeignKey('locations.id') unique=False, nullable=False)
+    user_id = db.Column(db.Boolean(), db.ForeignKey('user.id'), unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "characters": self.characters,
+            "locations": self.locations,
+            "user_id": self.user_id,
             # do not serialize the password, its a security breach
         }
